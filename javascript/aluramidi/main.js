e20e6267b587167c12a3d3823029e81eb0842a93
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-04-10 19:41:15 -0300

    :sparkles: Feat: Implementado uso de teclas para acionar eventos
    - Função keydown e keyup adicionada
    
    :recycle: refactor: função tocaSom.

--- javascript/aluramidi/main.js
+++ javascript/aluramidi/main.js
@@ -2,12 +2,52 @@ const teclas = document.querySelectorAll('.tecla');
 const sons = document.querySelectorAll('audio');
 const teclado = document.querySelector('.teclado');
 
-teclado.addEventListener('click', function (event) {
-    const tecla = event.target;
-    for (let index = 0; index < teclas.length; index++) {
-        if (tecla === teclas[index]) {
-            sons[index].play();
-            return;
-        }
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento || elemento.localName != "audio") {
+        elemento.play();
+    } else {
+        console.log("Elemento não encontrado ou seletor inválido");
     }
-}); 
+}
+
+// Meu código
+teclado.addEventListener('click', (event) => {
+    const tecla = event.target;
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tocaSom(idAudio);
+});
+
+teclado.addEventListener('keydown', (event) => {
+
+    if (event.key === "Enter" || event.key === "Space") {
+        event.target.classList.add("ativa");
+    }
+});
+
+teclado.addEventListener('keyup', (event) => {
+
+    if (event.key === "Enter" || event.key === "Space") {
+        event.target.classList.remove("ativa");
+    }
+});
+
+
+
+// const listaDeTeclas = document.querySelectorAll('.tecla');
+
+// let contador = 0;
+
+// while (contador < listaDeTeclas.length) {
+//     const tecla = listaDeTeclas[contador];
+//     const instrumento = tecla.classList[1];
+//     const idAudio = `#som_${instrumento}`
+
+//     tecla.onclick = function () {
+//         tocaSom(idAudio);
+//     };
+//     contador++;
+// }
