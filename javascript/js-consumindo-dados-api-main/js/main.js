0dd76c5231a8e864c1ca0841d641b89eb9100404
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-04-29 19:20:50 -0300

    :sparkles: Feat: Validação de formulário com API
    Utilizado API viaCEP para preencher e validar endereço do usuário.

--- javascript/js-consumindo-dados-api-main/js/main.js
+++ javascript/js-consumindo-dados-api-main/js/main.js
@@ -1,23 +1,44 @@
 async function buscaCEP(cep) {
+    const $erro = document.querySelector('#erro');
+    
     try {
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const data = await response.json();
-        if (response.erro) {
+
+        if (!response.ok) {
+            throw new Error(500)
+        } else if (data.erro) {
             throw new Error(404);
         } else {
-            console.log(data);
+            const $logradouro = document.querySelector('#endereco');
+            const $bairro = document.querySelector('#bairro');
+            const $localidade = document.querySelector('#cidade');
+            const $uf = document.querySelector('#estado');
+
+            $logradouro.value = data.logradouro;
+            $bairro.value = data.bairro;
+            $localidade.value = data.localidade;
+            $uf.value = data.uf;
+
+            $erro.innerHTML = '';
         }
     } catch (erro) {
         erro = erro.toString().replace('Error: ', '');
+
+        console.log(erro);
+
         switch (erro) {
             case "404":
+                $erro.innerHTML = `CEP não encontrado`;
                 console.log('CEP não encontrado');
                 break;
             case "500":
+                $erro.innerHTML = 'Erro no servidor';
                 console.log('Erro no servidor');
                 break;
             default:
-                console.log('CEP invalido');
+                $erro.innerHTML = 'CEP invalido';
+                console.log('CEP inválido');
                 break;
         }
     }
@@ -25,7 +46,7 @@ async function buscaCEP(cep) {
 
 const $cep = document.querySelector('#cep');
 
-$cep.addEventListener('focusout', buscaCEP($cep.value));
+$cep.addEventListener('focusout', () => buscaCEP($cep.value));
 
 
 
