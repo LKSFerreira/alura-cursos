c15eb647fb9eb79a881613841e98c6dec48f9050
LKSFerreira <lks_ferreira@hotmail.com>
2023-05-08 22:28:59 -0300

    :tada: POO em C#

---
+++ charp/bytebank/Program.cs
@@ -0,0 +1,14 @@
+﻿using bytebank;
+
+ContaCorrente contaDoLucas = new ContaCorrente();
+contaDoLucas.numeroAgencia = 863;
+contaDoLucas.conta = "863146";
+contaDoLucas.titular = "Bruno";
+contaDoLucas.saldo = 100;
+
+contaDoLucas.Depositar(500);
+Console.WriteLine($"Saldo da conta do Bruno: {contaDoLucas.saldo}");
+
+bool sacou = contaDoLucas.Sacar(800);
+Console.WriteLine($"Saque realizado com sucesso? {sacou}");
+Console.WriteLine($"Saldo da conta do Bruno: {contaDoLucas.saldo}");
